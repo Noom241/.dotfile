5d6ee027273aafd10866b26912620876ae44dc17
Noom241 <joaquin12345.007.2002@gmail.com>
2023-10-10 01:03:58 -0500

    Agregado

--- Install.sh
+++ Install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Ruta al archivo de lista de paquetes
-package_list="packageList.txt"
+# Lista de paquetes a instalar
+packages=("stow" "zsh" "neofetch" "picom" "kitty" "code" "nautilus" "feh" "firefox")
 
 # Ruta al directorio donde se encuentran los archivos de configuración para stow
 stow_dir="$HOME/.dotfile/"
 
-# Leer la lista de paquetes desde el archivo
-while read -r package; do
+# Instalar los paquetes
+for package in "${packages[@]}"; do
     # Instalar el paquete usando sudo pacman -S
     echo "Instalando el paquete: $package"
     sudo pacman -S --needed --noconfirm "$package"
-done < "$package_list"
+done
 
 # Usar stow para vincular archivos de configuración
 cd "$stow_dir"
